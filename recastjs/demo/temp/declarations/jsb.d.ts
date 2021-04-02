f007246e1510aa8a8795191b637a98d3c1eb58a7
DESKTOP-M4FPHOU\XplayGame2 <772055549@qq.com>
2021-04-02 19:07:23 +0800

    删除无用资源

--- recastjs/demo/temp/declarations/jsb.d.ts
+++
@@ -1,1 +0,0 @@
-/// <reference path="D:\cocos\CocosDashboard\resources\.editors\Creator\3.0.0\resources\resources\3d\engine\@types\jsb.d.ts"/>
--- recastjs/demo/temp/programming/packer-driver/targets/preview/mods/cce/internal/x/prerequisite-imports.js
+++
@@ -1,9 +0,0 @@
-System.register(["file:///D:/BabylonJsExtensions/recastjs/demo/assets/script/recastdetourjs/tool/INavigationEngine.ts", "file:///D:/BabylonJsExtensions/recastjs/demo/assets/script/recastdetourjs/tool/RecastDetourManager.ts", "file:///D:/BabylonJsExtensions/recastjs/demo/assets/script/recastdetourjs/tool/recastJsPlugin.ts", "file:///D:/BabylonJsExtensions/recastjs/demo/assets/script/test.ts"], function (_export, _context) {
-  "use strict";
-
-  return {
-    setters: [function (_fileDBabylonJsExtensionsRecastjsDemoAssetsScriptRecastdetourjsToolINavigationEngineTs) {}, function (_fileDBabylonJsExtensionsRecastjsDemoAssetsScriptRecastdetourjsToolRecastDetourManagerTs) {}, function (_fileDBabylonJsExtensionsRecastjsDemoAssetsScriptRecastdetourjsToolRecastJsPluginTs) {}, function (_fileDBabylonJsExtensionsRecastjsDemoAssetsScriptTestTs) {}],
-    execute: function () {}
-  };
-});
-//# sourceMappingURL=prerequisite-imports.js.map
